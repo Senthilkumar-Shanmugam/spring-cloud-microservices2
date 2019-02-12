6fd4647f4ed06cf960fb3a6b792d325e539fa639
tribles <senthilkumar.s.shanmugam@oracle.com>
2019-02-12 21:58:23 +0530

    added eureka server

--- spring-cloud-microservices2/catalog-service/src/main/java/org/catalog/service/CatalogServiceApplication.java
+++ spring-cloud-microservices2/catalog-service/src/main/java/org/catalog/service/CatalogServiceApplication.java
@@ -2,21 +2,21 @@ package org.catalog.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+/*import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
+*/
 @SpringBootApplication
-@EnableCircuitBreaker
-@EnableFeignClients
+//@EnableCircuitBreaker
+//@EnableFeignClients
 public class CatalogServiceApplication {
-	@Bean
+	/*@Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
+    }*/
 
 
     public static void main(String[] args) {
--- spring-cloud-microservices2/catalog-service/src/main/java/org/catalog/service/service/InventoryServiceFeignClient.java
+++ spring-cloud-microservices2/catalog-service/src/main/java/org/catalog/service/service/InventoryServiceFeignClient.java
@@ -3,10 +3,10 @@ package org.catalog.service.service;
 import java.util.List;
 
 import org.catalog.service.web.model.ProductInventoryResponse;
-import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "inventory-service")
+//@FeignClient(name = "inventory-service")
 public interface InventoryServiceFeignClient {
 	@GetMapping("/api/inventory")
     List<ProductInventoryResponse> getInventoryLevels();
